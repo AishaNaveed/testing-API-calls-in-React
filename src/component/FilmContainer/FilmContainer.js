b5f02f6e5c53f1a6087578acb93adf1957035d80
AishaNaveed <aisha@ailogica.com>
2021-12-03 09:51:57 +0000

    added unit test for title

--- src/component/FilmContainer/FilmContainer.js
+++ src/component/FilmContainer/FilmContainer.js
@@ -1,8 +1,5 @@
 const FilmContainer = ({ film }) => {
-    // console.log(film.length);
-    // if(film.length > 0)
-    //     console.log(film[0].title);
-    const objFilm = film[0];
+    
     return (
         <>
             <section className="card__container">
--- src/component/FilmContainer/FilmContainer.test.js
+++ src/component/FilmContainer/FilmContainer.test.js
@@ -6,13 +6,15 @@ import { setupServer } from "msw/node";
 let propsRequired;
 const server = setupServer(
     rest.get('https://ghibliapi.herokuapp.com/films', (req, res, ctx) => {
-        return (res(ctx.json([
-            {
-                title: "Castle in the Sky",
-                Image: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg",
-                description: "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world."
-            }
-        ])))
+        return (res(ctx.json({
+            film: [
+                {
+                    title: "Castle in the Sky",
+                    Image: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg",
+                    description: "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world."
+                }
+            ]
+        })))
     })
 );
 
@@ -20,23 +22,23 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 beforeEach(() => {
     propsRequired = {
-        title: "Castle in the Sky",
-        // url: "https://ghibliapi.herokuapp.com/films/",
+        film: [{
+            title: "Castle in the Sky",
+        }]
     };
 });
 afterAll(() => server.close());
 
 describe('Film Container component', () => {
     test('should render first film title on the page', async () => {
-        render(<FilmContainer {...propsRequired}/>);
+        render(<FilmContainer {...propsRequired} />);
         await waitFor(() => screen.getByRole('heading'));
-        expect(screen.getByRole('heading')).not.toHaveTextContent('Castle in the sky');
+        expect(screen.getByRole('heading')).toHaveTextContent(/Castle in the Sky/i);
     });
 
-    // test('should render first film title on the page', async () => {
-    //     render(<FilmContainer {...propsRequired}/>);
-    //     await waitFor(() => screen.getByRole('heading'));
-    //     expect(screen.getByText(/Castle in the sky/i)).toBeInTheDocument();
-    // });
-
+    test('should render first film title on the page', async () => {
+        render(<FilmContainer {...propsRequired} />);
+        expect(await waitFor(() =>
+            screen.getByText(/Castle in the sky/i))).toBeInTheDocument();
+    });
 });
